452ff3a0dc2acccf1448bf2051af87accc4518f7
Cazeho <58745332+Cazeho@users.noreply.github.com>
2022-09-19 09:55:14 +0200

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,6 @@
+
+
+# download Virtualbox & Vagrant
+
+wget "https://download.virtualbox.org/virtualbox/6.1.38/VirtualBox-6.1.38-153438-Win.exe" -outfile "virtualbox.exe"
+wget "https://releases.hashicorp.com/vagrant/2.3.0/vagrant_2.3.0_windows_amd64.msi" -outfile "vagrant.exe"
